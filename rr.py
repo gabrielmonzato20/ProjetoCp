72e48a11dcce2056d11fa1f4624b674dccf4e55b
Ronald Huan Rocha Pereira <1801147@SALAS.AULAS>
2018-08-22 11:16:46 -0300

    Alteracao tabuada

--- rr.py
+++ rr.py
@@ -1,2 +1,9 @@
 def rr():
     print("RR is live !!!!!!")
+
+def tabuada(num):
+    for x in range(1, 11):
+        tab = num * x
+        print(f"{num}x{x} = {tab}")
+tabuada(int(input("Digite um número para ver a tabuada: ")))
+

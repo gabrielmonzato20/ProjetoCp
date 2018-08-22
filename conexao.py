a0d6a3204487d653669284f651c911c09386d626
family335 <1701872@SALAS.AULAS>
2018-08-22 11:20:21 -0300

    'alter2'

--- conexao.py
+++ conexao.py
@@ -1,2 +1,11 @@
 def mensagem():
     print('Criando no python')
+
+def tabuada():
+    n = int(input('Digite um número que deseja ver a tabuada: '))
+    for x in range (1,11):
+        print('{}  X {:2} = {:2}'.format(n, x, n*x))
+
+mensagem()
+tabuada()
+  

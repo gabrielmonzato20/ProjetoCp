466bc974871d9d10da32846eece4f931243cb1f9
zagoewertom <ewertom_zago@hotmail.com>
2018-08-22 10:49:01 -0300

    AltTom

--- rr.py
+++ rr.py
@@ -1,2 +1,14 @@
 def rr():
-    print("RR is live !!!!!!")
+    print("RR is LIVES !!!!!!")
+
+def tt():
+    li1=[1,2,3,4,5,6,4,5,6]
+    li2=[7,8,8,9,5,4,6,2,1]
+    li3=[]
+    for c in range(len(li1)):
+            soma =0 
+            soma = li1[c]+li2[c]
+            li3.append(soma)
+   
+    print(li3)
+tt()
